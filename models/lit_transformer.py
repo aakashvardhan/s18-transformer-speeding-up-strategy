6d7e5a561a64b7c945d4fe9c88ef9ed3c079d4f2
aakashvardhan <vardhan.aakash1@gmail.com>
2024-06-11 16:27:15 +0400

    Refactor config_file.py to update batch size to 16
    The code changes in config_file.py modify the get_config function to update the batch_size value from 32 to 16. This change reduces the batch size used during training, potentially improving memory usage and training performance.

--- models/lit_transformer.py
+++ models/lit_transformer.py
@@ -1,128 +1,123 @@
 import os
-
-import lightning as L
+import random
 import torch
 import torch.nn as nn
+import torch.optim as optim
 from torchmetrics.text import BLEUScore, CharErrorRate, WordErrorRate
-from models.transformer import ProjectionLayer, Transformer, build_transformer
+import lightning as L
+from models.transformer import build_transformer
 from utils import casual_mask
-
-import random
-
+from config_file import get_weights_file_path
+from dataset import get_ds
 
 def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len):
+    """
+    Greedy decoding for sequence generation.
 
+    Args:
+        model (nn.Module): The transformer model.
+        source (Tensor): Source input tensor.
+        source_mask (Tensor): Source mask tensor.
+        tokenizer_src (Tokenizer): Source tokenizer.
+        tokenizer_tgt (Tokenizer): Target tokenizer.
+        max_len (int): Maximum length of the generated sequence.
+
+    Returns:
+        Tensor: Decoded sequence.
+    """
     sos_idx = tokenizer_tgt.token_to_id("[SOS]")
     eos_idx = tokenizer_tgt.token_to_id("[EOS]")
-
     encoder_output = model.encode(source, source_mask)
-    # Initialize the decoder input with SOS token
     decoder_input = torch.empty(1, 1).fill_(sos_idx).type_as(source)
+
     while True:
         if decoder_input.size(1) == max_len:
             break
         decoder_mask = casual_mask(decoder_input.size(1)).type_as(source_mask)
         out = model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
-
         prob = model.project(out[:, -1])
         _, next_word = torch.max(prob, dim=1)
-        decoder_input = torch.cat(
-            [
-                decoder_input,
-                torch.empty(1, 1).type_as(source_mask).fill_(next_word.item()),
-            ],
-            dim=1,
-        )
-
+        decoder_input = torch.cat([decoder_input, torch.empty(1, 1).type_as(source_mask).fill_(next_word.item())], dim=1)
         if next_word == eos_idx:
             break
 
     return decoder_input.squeeze(0)
 
-
 def get_model(config, src_vocab_size, tgt_vocab_size):
-    model = build_transformer(
-        src_vocab_size,
-        tgt_vocab_size,
-        config["seq_len"],
-        config["seq_len"],
-        d_model=config["d_model"],
-    )
-    return model
-
-
-class LT_model(L.LightningModule):
     """
-    LightningModule for the LT_model.
+    Build the transformer model.
+
+    Args:
+        config (dict): Configuration parameters.
+        src_vocab_size (int): Source vocabulary size.
+        tgt_vocab_size (int): Target vocabulary size.
+
+    Returns:
+        nn.Module: Transformer model.
+    """
+    return build_transformer(src_vocab_size, tgt_vocab_size, config["seq_len"], config["seq_len"], d_model=config["d_model"])
+
+class LTModel(L.LightningModule):
+    """
+    LightningModule for the LTModel.
 
     Args:
         config (dict): Configuration parameters for the model.
-        tokenizer_src (Tokenizer): Source tokenizer.
-        tokenizer_tgt (Tokenizer): Target tokenizer.
+        one_cycle_best_lr (float): Best learning rate for one cycle learning rate scheduler.
     """
-
-    def __init__(self, config, tokenizer_src, tokenizer_tgt, one_cycle_best_LR=0.01):
+    def __init__(self, config, one_cycle_best_lr=0.01):
         super().__init__()
         self.config = config
-        self.tokenizer_src = tokenizer_src
-        self.tokenizer_tgt = tokenizer_tgt
+        _, _, self.tokenizer_src, self.tokenizer_tgt = get_ds(config)
         self.src_vocab_size = self.tokenizer_src.get_vocab_size()
         self.tgt_vocab_size = self.tokenizer_tgt.get_vocab_size()
         self.model = get_model(config, self.src_vocab_size, self.tgt_vocab_size)
-        
         self.learning_rate = config["lr"]
-        self.one_cycle_best_LR = one_cycle_best_LR
-
+        self.one_cycle_best_lr = one_cycle_best_lr
         self.source_texts = []
         self.expected = []
         self.predicted = []
-        
         self.training_step_outputs = []
-
         self.cer_metric = CharErrorRate()
         self.wer_metric = WordErrorRate()
         self.bleu_metric = BLEUScore()
-
         self.save_hyperparameters()
 
     def forward(self, batch):
         """
-        Forward pass of the LT_model.
+        Forward pass of the LTModel.
 
         Args:
-            encoder_input (Tensor): Input to the encoder.
-            decoder_input (Tensor): Input to the decoder.
-            encoder_mask (Tensor): Mask for the encoder input.
-            decoder_mask (Tensor): Mask for the decoder input.
+            batch (dict): Batch of data containing encoder and decoder inputs and masks.
 
         Returns:
             Tensor: Projected output of the model.
         """
-
-        encoder_input = batch["encoder_input"]  # (batch_size, seq_len)
-        decoder_input = batch["decoder_input"]  # (batch_size, seq_len)
-
+        encoder_input = batch["encoder_input"]
+        decoder_input = batch["decoder_input"]
         encoder_mask = batch["encoder_mask"]
         decoder_mask = batch["decoder_mask"]
-
         encoder_output = self.model.encode(encoder_input, encoder_mask)
-        decoder_output = self.model.decode(
-            encoder_output, encoder_mask, decoder_input, decoder_mask
-        )
-        proj_output = self.model.project(decoder_output)
-        return proj_output
+        decoder_output = self.model.decode(encoder_output, encoder_mask, decoder_input, decoder_mask)
+        return self.model.project(decoder_output)
 
     def loss_fn(self, proj_output, label):
-        # Define the loss function
-        loss_fn = nn.CrossEntropyLoss(
-            ignore_index=self.tokenizer_tgt.token_to_id("[PAD]"), label_smoothing=0.1
-        )
-        loss = loss_fn(proj_output.view(-1, self.tgt_vocab_size), label.view(-1))
-        return loss
+        """
+        Compute the loss for the model.
+
+        Args:
+            proj_output (Tensor): Projected output from the model.
+            label (Tensor): Ground truth labels.
+
+        Returns:
+            Tensor: Computed loss.
+        """
+        loss_fn = nn.CrossEntropyLoss(ignore_index=self.tokenizer_tgt.token_to_id("[PAD]"), label_smoothing=0.1)
+        return loss_fn(proj_output.view(-1, self.tgt_vocab_size), label.view(-1))
 
     def training_step(self, batch, batch_idx):
         """
-        Training step of the LT_model.
+        Training step for the LTModel.
 
         Args:
             batch (dict): Batch of data.
@@ -132,72 +127,67 @@ class LT_model(L.LightningModule):
             Tensor: Loss value.
         """
         proj_output = self(batch)
-        label = batch["label"]  # (batch_size, seq_len)
-        loss = self.loss_fn(proj_output, label)
-        self.log_dict(
-            {"train_loss": loss},
-            on_step=True,
-            on_epoch=True,
-            prog_bar=True,
-            sync_dist=True,
-            logger=True,
-        )
+        loss = self.loss_fn(proj_output, batch["label"])
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         self.training_step_outputs.append(loss)
-
         return loss
 
+    def on_train_start(self):
+        """
+        Actions to perform at the start of training.
+        """
+        if self.config['preload']:
+            model_filename = get_weights_file_path(self.config, self.config['preload'])
+            state = torch.load(model_filename)
+            self.model.load_state_dict(state['model_state_dict'])
+            self.initial_epoch = state['epoch'] + 1
+            self.optimizer.load_state_dict(state['optimizer_state_dict'])
+            print("Preloaded")
+
     def on_train_epoch_start(self):
-        # Clear or reset the training_step_outputs at the beginning of each epoch
+        """
+        Actions to perform at the start of each training epoch.
+        """
         self.training_step_outputs.clear()
 
     def on_train_epoch_end(self):
+        """
+        Actions to perform at the end of each training epoch.
+        """
         if self.training_step_outputs:
             epoch_mean_loss = torch.stack(self.training_step_outputs).mean()
-            self.log("train_loss", epoch_mean_loss, on_epoch=True)
+            self.log("train_loss_epoch", epoch_mean_loss, on_epoch=True)
         else:
             print("Warning: No outputs to stack for epoch mean loss calculation.")
 
     def on_load_checkpoint(self, checkpoint):
-        # Clear or restore self.training_step_outputs as needed
+        """
+        Actions to perform when loading a checkpoint.
+
+        Args:
+            checkpoint (dict): Checkpoint dictionary.
+        """
         self.training_step_outputs.clear()
 
     def configure_optimizers(self):
         """
-        Configure the optimizer for the LT_model.
+        Configure the optimizer and learning rate scheduler.
 
         Returns:
-            Optimizer: The optimizer.
+            list: List containing the optimizer and learning rate scheduler.
         """
-        optimizer = torch.optim.Adam(
-            self.model.parameters(), lr=self.learning_rate, eps=1e-9
-        )
-
+        optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate, eps=1e-9)
         dataloader = self.trainer.datamodule.train_dataloader()
-
-        scheduler = torch.optim.lr_scheduler.OneCycleLR(
-            optimizer,
-            max_lr=self.one_cycle_best_LR,
-            steps_per_epoch=len(dataloader),
-            epochs=self.trainer.max_epochs,
-            pct_start=0.2,
-            div_factor=100,
-            three_phase=False,
-            final_div_factor=100,
-            anneal_strategy="linear",
+        scheduler = optim.lr_scheduler.OneCycleLR(
+            optimizer, max_lr=self.one_cycle_best_lr, steps_per_epoch=len(dataloader),
+            epochs=self.trainer.max_epochs, pct_start=0.2, div_factor=100, three_phase=False,
+            final_div_factor=100, anneal_strategy="linear"
         )
-
-        lr_scheduler = {
-            "scheduler": scheduler,
-            "interval": "step",
-            "frequency": 1,
-            "monitor": "train_loss",
-        }
-
-        return [optimizer], [lr_scheduler]
+        return [optimizer], [{"scheduler": scheduler, "interval": "step", "frequency": 1, "monitor": "train_loss"}]
 
     def validation_step(self, batch, batch_idx):
         """
-        Validation step of the LT_model.
+        Validation step for the LTModel.
 
         Args:
             batch (dict): Batch of data.
@@ -206,45 +196,26 @@ class LT_model(L.LightningModule):
         Returns:
             dict: Dictionary containing validation metrics.
         """
-
         encoder_input = batch["encoder_input"]
         encoder_mask = batch["encoder_mask"]
         assert encoder_input.size(0) == 1, "Batch size must be 1 for validation"
-        model_out = greedy_decode(
-            self.model,
-            encoder_input,
-            encoder_mask,
-            self.tokenizer_src,
-            self.tokenizer_tgt,
-            self.config["seq_len"],
-        )
+        model_out = greedy_decode(self.model, encoder_input, encoder_mask, self.tokenizer_src, self.tokenizer_tgt, self.config["seq_len"])
         model_out_text = self.tokenizer_tgt.decode(model_out.detach().cpu().numpy())
-        source_text = batch["src_text"][0]
-        target_text = batch["tgt_text"][0]
-
-        self.source_texts.append(source_text)
-        self.expected.append(target_text)
+        self.source_texts.append(batch["src_text"][0])
+        self.expected.append(batch["tgt_text"][0])
         self.predicted.append(model_out_text)
 
     def on_validation_epoch_end(self):
-
-        # Compute validation metrics
+        """
+        Actions to perform at the end of each validation epoch.
+        """
         cer = self.cer_metric(self.predicted, self.expected)
         wer = self.wer_metric(self.predicted, self.expected)
         bleu = self.bleu_metric(self.predicted, self.expected)
-
         for _ in range(self.config["num_examples"]):
             idx = random.randint(0, len(self.source_texts) - 1)
             print("-" * 80)
             print(f"{f'SOURCE: ':>12}{self.source_texts[idx]}")
             print(f"{f'TARGET: ':>12}{self.expected[idx]}")
             print(f"{f'PREDICTED: ':>12}{self.predicted[idx]}")
-
-        # Log the validation loss dictionary
-        self.log_dict(
-            {"val_cer": cer, "val_wer": wer, "val_bleu": bleu},
-            on_step=False,
-            on_epoch=True,
-            prog_bar=True,
-            sync_dist=True,
-        )
+        self.log_dict({"val_cer": cer, "val_wer": wer, "val_bleu": bleu}, on_epoch=True, prog_bar=True, logger=True)
--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import os
 import warnings
 from pathlib import Path
+
 import torch
 import torchmetrics
 import lightning as L
@@ -11,8 +12,6 @@ from lightning.pytorch.callbacks import (
     EarlyStopping,
     TQDMProgressBar,
 )
-
-
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
@@ -22,22 +21,20 @@ from torch.optim.lr_scheduler import LambdaLR
 from torch.utils.data import DataLoader, Dataset, random_split
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
+
 from config_file import get_config, get_weights_file_path
 from dataset import BillingualDataset, LT_DataModule
-
 from utils import dynamic_collate_fn, casual_mask
-
 import torch.nn as nn
 from models.lit_transformer import LT_model
 from models.transformer import build_transformer
 
-
+# Clear CUDA cache and set environment variable
 torch.cuda.empty_cache()
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:12240"
+
+# Load configuration
 config = get_config()
-
-
-
 
 def main(cfg, ckpt_file=None, if_ckpt=False):
     """
@@ -48,6 +45,7 @@ def main(cfg, ckpt_file=None, if_ckpt=False):
         ckpt_file (str): Path to the checkpoint file.
         if_ckpt (bool, optional): Whether to load the model from a checkpoint. Defaults to False.
     """
+    # Clear CUDA cache and set seed
     torch.cuda.empty_cache()
     L.seed_everything(42, workers=True)
     print("Seed set to 42...")
@@ -55,21 +53,14 @@ def main(cfg, ckpt_file=None, if_ckpt=False):
     # Initialize the data module
     datamodule = LT_DataModule(cfg)
     datamodule.setup()
-    tk_src = datamodule.tokenizer_src  # Assuming datamodule is your instance of BilingualDataModule
-    tk_tgt = datamodule.tokenizer_tgt
-
-    src_vocab_size = tk_src.get_vocab_size()
-    tgt_vocab_size = tk_tgt.get_vocab_size()
-
     print("DataModule initialized...")
 
-    # Tensorboard logger
+    # Initialize TensorBoard logger
     tb_logger = TensorBoardLogger(
         save_dir=os.getcwd(), version=1, name="lightning_logs"
     )
 
     # Initialize the trainer
-
     trainer = L.Trainer(
         precision=cfg["precision"],
         max_epochs=cfg["num_epochs"],
@@ -83,7 +74,7 @@ def main(cfg, ckpt_file=None, if_ckpt=False):
                 save_top_k=3,
                 monitor="train_loss",
                 mode="min",
-                filename="model-{epoch:02d}-{train_loss:4f}",
+                filename="model-{epoch:02d}-{train_loss:.4f}",
                 save_last=True,
             ),
             LearningRateMonitor(logging_interval="step", log_momentum=True),
@@ -98,44 +89,39 @@ def main(cfg, ckpt_file=None, if_ckpt=False):
         check_val_every_n_epoch=2,
         limit_val_batches=1000,
     )
-    
-    tuner = L.pytorch.tuner.Tuner(trainer)
-    
+
     # Initialize the model
-    model = LT_model(cfg, tk_src, tk_tgt)
-    
-    lr_finder = tuner.lr_find(
-        model, datamodule=datamodule, num_training=trainer.max_epochs
-    )
+    model = LT_model(cfg)
+
+    # Learning rate finder
+    tuner = L.pytorch.tuner.Tuner(trainer)
+    lr_finder = tuner.lr_find(model, datamodule=datamodule, num_training=trainer.max_epochs)
     print(lr_finder)
-    
-    # Check if the lr_finder has completed successfully
+
     if lr_finder:
-        # Plot with suggest=True to find the suggested learning rate
         fig = lr_finder.plot(suggest=True)
         fig.show()
-
-        # Get the suggested learning rate
         suggested_lr = lr_finder.suggestion()
         print(f"Suggested learning rate: {suggested_lr}")
     else:
         print("Learning rate finding did not complete successfully.")
 
+    # Set the best learning rate
+    model.one_cycle_best_lr = suggested_lr
+
     # Train the model
-
-    model.one_cycle_best_LR = suggested_lr
-
     if if_ckpt:
         trainer.fit(model=model, datamodule=datamodule, ckpt_path=ckpt_file)
     else:
         trainer.fit(model=model, datamodule=datamodule)
 
+    # Validate the model
     trainer.validate(model=model, datamodule=datamodule)
     print("Model Evaluation Done...")
 
     # Save the model
-    torch.save(
-        model.state_dict(),
-        "saved_resnet18_model.pth",
-    )
+    torch.save(model.state_dict(), "saved_resnet18_model.pth")
     print("Model saved...")
+
+if __name__ == "__main__":
+    main(config)
