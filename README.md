9cf42b749a59d90606decc957241e9c382588129
aakashvardhan <vardhan.aakash1@gmail.com>
2024-06-18 13:06:13 +0400

    Refactor code to optimize training of Transformer model

--- README.md
+++ README.md
@@ -1,8 +1,58 @@
+# Speeding up the training of the Transformer model with optimization techniques
 
+## Table of Contents
+1. [Introduction](#introduction)
+2. [Key Improvements](#key-improvements)
+3. [Results](#results)
+4. [Conclusion](#conclusion)
+
+## Introduction
+
+The Transformation model is known for its parallelization capabilities and its ability to capture long-range dependencies in sequences. However, training the Transformer model can be computationally expensive due to the large number of parameters and the complexity of the model architecture. In this project, we explore various optimization techniques to speed up the training of the Transformer model. We experiment with various optimization techniques to reduce the training time of the model. We evaluate the performance of these techniques on a machine translation task using the opus-book English to Italian dataset.
+
+## Key Improvements
+
+We experiment with the following optimization techniques to speed up the training of the Transformer model:
+
+- **16-bit Mixed Precision Training**: We use mixed precision training to speed up the training of the model by reducing the memory footprint and increasing the computation speed. Using Pytorch lightning, we can easily enable mixed precision training by setting the `precision=16-mixed` flag in the `Trainer` class.
+
+- **Gradient Clipping**: We use gradient clipping to prevent the exploding gradient problem during training. We set the maximum gradient norm to 0.5 to prevent the gradients from becoming too large. Using Pytorch lightning, we can easily enable gradient clipping by setting the `gradient_clip_val=0.5` flag in the `Trainer` class.
+
+- **OneCycle LR Scheduler**: The OneCycleLR scheduler is a learning rate policy designed to improve the training efficiency and performance of neural networks, including transformer models. This technique was proposed in the paper "Super-Convergence: Very Fast Training of Neural Networks Using Large Learning Rates". This approach dynamically adjusts the learning rate during training to speed up the convergence of the model. We use the OneCycleLR scheduler with a maximum learning rate of 5e-4 and a total number of training steps of the length of the `train_dataloader`.
+
+- **Stacking Multiple Encoder and Decoder Layers**: We experiment with stacking multiple encoder and decoder layers to increase the model's capacity and improve its performance. We increase the number of encoder and decoder layers by 6 to enhance the model's ability to capture complex patterns and improve the translation quality.
+
+- **Dynamic Batch Padding**: This is a crucial component for training a transformer model on tasks with variable-length sequences. We use dynamic batch padding to ensure the input sequences are padded to the same length within each batch. This technique helps speed up the training process by reducing the number of computations required to process each batch.
+
+## Results
+
+We evaluate the performance of the Transformer model with the optimization techniques on the opus-book English to Italian dataset. We train the model for 1 epochs and evaluate the translation quality using the BLEU score metric. The results show that the optimization techniques significantly speed up the training process and improve the translation quality of the model.
+
+Final Training and Validation Results:
+
+```
+  | Name    | Type             | Params | Mode 
+-----------------------------------------------------
+0 | model   | Transformer      | 34.9 M | train
+1 | loss_fn | CrossEntropyLoss | 0      | train
+-----------------------------------------------------
+34.9 M    Trainable params
+0         Non-trainable params
+34.9 M    Total params
+139.716   Total estimated model params size (MB)
+Max length of the source sentence : 43
+Max length of the source target : 38
+
+Epoch 16: 100%
+ 241/241 [00:33<00:00,  7.20it/s, v_num=1, train_loss_step=1.580, train_loss_epoch=1.580]
+```
+
+- With Pytorch Lightning's Early Stopping Callback, the training process was stopped after 16 epochs as the validation loss reached the 1.5 threshold.
 
 
 16th Epoch Validation Result:
 
+```
 --------------------------------------------------------------------------------
     SOURCE: And he loves me; All this has been, but will pass,' she said, feeling that tears of joy at this return to life were running down her cheeks.
     TARGET: Perché lui mi ama! Questo è stato e passerà” ella diceva, sentendo che le lacrime della gioia del ritorno alla vita le scorrevano per le guance.
@@ -11,5 +61,22 @@
     SOURCE: "Mr. Rivers! you quite put me out of patience: I am rational enough; it is you who misunderstand, or rather who affect to misunderstand."
     TARGET: — Signor Rivers, mi farete perder la pazienza; sono calma; siete voi che non capite, o che fingete di non capirmi.
  PREDICTED: — Il signor Rivers era suo aiuto , che ora mi è ora di voi ; ma voi chi è a sbagliare e voi al mio .
+--------------------------------------------------------------------------------
+    SOURCE: 'What is it?' he asked drily. 'We are busy.'
+    TARGET: — Che vuoi? — le disse asciutto. — Siamo occupati.
+ PREDICTED: — Che vuoi ? — le disse asciutto . — Siamo .
+```
 
- 
+```html
+<div class="grid-container">
+    <div class="grid-item">
+        <img src="image1.jpg" alt="Image 1">
+    </div>
+    <div class="grid-item">
+        <img src="image2.jpg" alt="Image 2">
+    </div>
+    <div class="grid-item">
+        <img src="image3.jpg" alt="Image 3">
+    </div>
+</div>
+```
--- main.py
+++ main.py
@@ -189,114 +189,114 @@ class LTModel(L.LightningModule):
         ]
 
 
-def main(cfg, ckpt_file=None, if_ckpt=False, debug=False):
-    """
-    Main function for training and evaluating a model.
+# def main(cfg, ckpt_file=None, if_ckpt=False, debug=False):
+#     """
+#     Main function for training and evaluating a model.
 
-    Args:
-        cfg (dict): Configuration parameters for the model training.
-        ckpt_file (str): Path to the checkpoint file.
-        if_ckpt (bool, optional): Whether to load the model from a checkpoint. Defaults to False.
-    """
-    # Define the directory name
-    directory_name = "weights"
+#     Args:
+#         cfg (dict): Configuration parameters for the model training.
+#         ckpt_file (str): Path to the checkpoint file.
+#         if_ckpt (bool, optional): Whether to load the model from a checkpoint. Defaults to False.
+#     """
+#     # Define the directory name
+#     directory_name = "weights"
 
-    # Create the directory if it does not exist
-    if not os.path.exists(directory_name):
-        os.makedirs(directory_name)
-        print(f"Directory '{directory_name}' created!")
-    else:
-        print(f"Directory '{directory_name}' already exists.")
+#     # Create the directory if it does not exist
+#     if not os.path.exists(directory_name):
+#         os.makedirs(directory_name)
+#         print(f"Directory '{directory_name}' created!")
+#     else:
+#         print(f"Directory '{directory_name}' already exists.")
 
-    # Clear CUDA cache and set seed
-    torch.cuda.empty_cache()
-    L.seed_everything(42, workers=True)
-    print("Seed set to 42...")
+#     # Clear CUDA cache and set seed
+#     torch.cuda.empty_cache()
+#     L.seed_everything(42, workers=True)
+#     print("Seed set to 42...")
 
-    # Initialize the data module
-    datamodule = LiTDataModule(cfg)
-    datamodule.setup()
-    print("DataModule initialized...")
-    tokenizer_src, tokenizer_tgt = datamodule.tokenizer_src, datamodule.tokenizer_tgt
-    # Initialize TensorBoard logger
-    tb_logger = TensorBoardLogger(
-        save_dir=os.getcwd(), version=1, name="lightning_logs"
-    )
+#     # Initialize the data module
+#     datamodule = LiTDataModule(cfg)
+#     datamodule.setup()
+#     print("DataModule initialized...")
+#     tokenizer_src, tokenizer_tgt = datamodule.tokenizer_src, datamodule.tokenizer_tgt
+#     # Initialize TensorBoard logger
+#     tb_logger = TensorBoardLogger(
+#         save_dir=os.getcwd(), version=1, name="lightning_logs"
+#     )
 
-    if debug:
-        trainer = L.Trainer(fast_dev_run=True, accelerator="cuda", devices="auto")
-        # Initialize the model
-        model = LTModel(cfg, tokenizer_src=tokenizer_src, tokenizer_tgt=tokenizer_tgt)
-        trainer.fit(model=model, datamodule=datamodule)
-        print("Debugging Done...")
-    else:
-        # Initialize the trainer
-        trainer = L.Trainer(
-            precision=cfg["precision"],
-            max_epochs=cfg["num_epochs"],
-            logger=tb_logger,
-            accelerator=cfg["accelerator"],
-            devices="auto",
-            default_root_dir=cfg["model_folder"],
-            callbacks=[
-                ModelCheckpoint(
-                    dirpath=cfg["model_folder"],
-                    save_top_k=3,
-                    monitor="train_loss_step",
-                    mode="min",
-                    filename="model-{epoch:02d}-{train_loss:.4f}",
-                    save_last=True,
-                ),
-                LearningRateMonitor(logging_interval="step", log_momentum=True),
-                EarlyStopping(
-                    monitor="train_loss_step", mode="min", stopping_threshold=1.6
-                ),
-                TQDMProgressBar(refresh_rate=10),
-            ],
-            gradient_clip_val=0.5,
-            num_sanity_val_steps=5,
-            enable_progress_bar=True,
-            check_val_every_n_epoch=1,
-            limit_val_batches=2,
-        )
+#     if debug:
+#         trainer = L.Trainer(fast_dev_run=True, accelerator="cuda", devices="auto")
+#         # Initialize the model
+#         model = LTModel(cfg, tokenizer_src=tokenizer_src, tokenizer_tgt=tokenizer_tgt)
+#         trainer.fit(model=model, datamodule=datamodule)
+#         print("Debugging Done...")
+#     else:
+#         # Initialize the trainer
+#         trainer = L.Trainer(
+#             precision=cfg["precision"],
+#             max_epochs=cfg["num_epochs"],
+#             logger=tb_logger,
+#             accelerator=cfg["accelerator"],
+#             devices="auto",
+#             default_root_dir=cfg["model_folder"],
+#             callbacks=[
+#                 ModelCheckpoint(
+#                     dirpath=cfg["model_folder"],
+#                     save_top_k=3,
+#                     monitor="train_loss_step",
+#                     mode="min",
+#                     filename="model-{epoch:02d}-{train_loss:.4f}",
+#                     save_last=True,
+#                 ),
+#                 LearningRateMonitor(logging_interval="step", log_momentum=True),
+#                 EarlyStopping(
+#                     monitor="train_loss_step", mode="min", stopping_threshold=1.6
+#                 ),
+#                 TQDMProgressBar(refresh_rate=10),
+#             ],
+#             gradient_clip_val=0.5,
+#             num_sanity_val_steps=5,
+#             enable_progress_bar=True,
+#             check_val_every_n_epoch=1,
+#             limit_val_batches=2,
+#         )
 
-        # Initialize the model
-        model = LTModel(cfg, tokenizer_src=tokenizer_src, tokenizer_tgt=tokenizer_tgt)
+#         # Initialize the model
+#         model = LTModel(cfg, tokenizer_src=tokenizer_src, tokenizer_tgt=tokenizer_tgt)
 
-        # Learning rate finder
-        tuner = L.pytorch.tuner.Tuner(trainer)
-        lr_finder = tuner.lr_find(
-            model, datamodule=datamodule, num_training=trainer.max_epochs
-        )
-        print(lr_finder)
+#         # Learning rate finder
+#         tuner = L.pytorch.tuner.Tuner(trainer)
+#         lr_finder = tuner.lr_find(
+#             model, datamodule=datamodule, num_training=trainer.max_epochs
+#         )
+#         print(lr_finder)
 
-        # Initialize suggested_lr with a default value
-        suggested_lr = cfg["one_cycle_best_lr"]
+#         # Initialize suggested_lr with a default value
+#         suggested_lr = cfg["one_cycle_best_lr"]
 
-        if lr_finder:
-            fig = lr_finder.plot(suggest=True)
-            fig.show()
-            suggested_lr = lr_finder.suggestion()
-            print(f"Suggested learning rate: {suggested_lr}")
-        else:
-            print("Learning rate finding did not complete successfully.")
+#         if lr_finder:
+#             fig = lr_finder.plot(suggest=True)
+#             fig.show()
+#             suggested_lr = lr_finder.suggestion()
+#             print(f"Suggested learning rate: {suggested_lr}")
+#         else:
+#             print("Learning rate finding did not complete successfully.")
 
-        # Set the best learning rate
-        model.one_cycle_best_lr = suggested_lr
+#         # Set the best learning rate
+#         model.one_cycle_best_lr = suggested_lr
 
-        # Train the model
-        if if_ckpt:
-            trainer.fit(model=model, datamodule=datamodule, ckpt_path=ckpt_file)
-        else:
-            trainer.fit(model=model, datamodule=datamodule)
+#         # Train the model
+#         if if_ckpt:
+#             trainer.fit(model=model, datamodule=datamodule, ckpt_path=ckpt_file)
+#         else:
+#             trainer.fit(model=model, datamodule=datamodule)
 
-        # Validate the model
-        trainer.validate(model=model, datamodule=datamodule)
-        print("Model Evaluation Done...")
+#         # Validate the model
+#         trainer.validate(model=model, datamodule=datamodule)
+#         print("Model Evaluation Done...")
 
-        # Save the model
-        torch.save(model.state_dict(), "saved_resnet18_model.pth")
-        print("Model saved...")
+#         # Save the model
+#         torch.save(model.state_dict(), "saved_resnet18_model.pth")
+#         print("Model saved...")
 
 
 # if __name__ == "__main__":
